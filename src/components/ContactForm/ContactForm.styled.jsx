d320080f43f3688f380b8cee333d7e7999b15e5e
Serhii Lapchenko <lapchenko.ss@gmail.com>
2022-10-31 15:30:24 +0000

    Update 3 files

--- src/components/ContactForm/ContactForm.styled.jsx
+++ src/components/ContactForm/ContactForm.styled.jsx
@@ -15,4 +15,23 @@ export const Input = styled.input`
 
 export const Button = styled.button`
   margin-bottom: 15px;
+  
+  width: 100%;
+  padding: 0.35em 1.2em;
+  background-color: grey;
+  border: 0.1em solid #ffffff;
+  margin-bottom: 15px;
+  border-radius: 0.12em;
+  
+  text-decoration: none;
+  font-size: 15px;
+  font-weight: 300;
+  color: #ffffff;
+  text-align: center;
+  transition: all 0.2s;
+  
+  :hover {
+    color: #ffffff;
+    background-color: #000000;
+  }
 `;
--- src/components/ContactList/ContactList.styled.jsx
+++ src/components/ContactList/ContactList.styled.jsx
@@ -5,5 +5,6 @@ export const ContactListGroup = styled.ul``;
 export const СontactСard = styled.li`
   display: flex;
   justify-content: space-between;
+  align-items: center;
   margin-bottom: 5px;
 `;
